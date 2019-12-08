40efe6c99f4e6c66ac946ed58b2b52bc65ec56fd
soupcan <greg@soupcan.org>
2019-12-08 11:27:49 -0500

    Removed WPFAppShell from namespace for everything in the SamplePlugin

--- WPFAppShell/SamplePlugin/Views/IncrementCommandView.xaml.cs
+++ WPFAppShell/SamplePlugin/Views/IncrementCommandView.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WPFAppShell.SamplePlugin.Views
+namespace SamplePlugin.Views
 {
     /// <summary>
     /// Interaction logic for IncrementCommandView.xaml
